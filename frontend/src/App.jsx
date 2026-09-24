54b8fa4524f0b8f2d685f59d041c66bb81c6e570
Antonio gutierrez <anthonygtz2703@gmail.com>
2026-09-23 21:03:08 -0600

    EXPEDIENTE CORREGIDO

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -962,7 +962,7 @@ const handleDownloadWord = async (expedienteId, datosParam = null) => {
     const downloadUrl = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = downloadUrl;
-    a.download = `Expediente_${idReal}.docx`;
+    a.download = `Expediente_${expedienteId}.docx`;
     document.body.appendChild(a);
     a.click();
     a.remove();
